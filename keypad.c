65cf24f71a11c14e22135cd44debb0fe89938965
unknown <comeau@TUCPD047.vpn.mmsi.com>
2014-10-06 15:09:22 -0700

    Lab2, Part1 - KeypadInitialize Function Complete

--- keypad.c
+++ keypad.c
@@ -10,6 +10,28 @@ void KeypadInitialize() {
 	// TODO: Configure IOs and Change Notificaiton interrupt for keypad scanning. This 
 	// configuration should ensure that if any key is pressed, a change notification interrupt 
 	// will be generated.
+
+        //Configure data direction
+        TRISBbits.TRISB2    = 1;
+        TRISBbits.TRISB6    = 1;
+        TRISBbits.TRISB7    = 1;
+        TRISBbits.TRISB8    = 0;
+        TRISBbits.TRISB9    = 0;
+        TRISBbits.TRISB10   = 0;
+        TRISBbits.TRISB11   = 0;
+
+        //Default pins to digital where neccessary.
+        AD1PCFGbits.PCFG2   = 1;
+
+        //Enable the change notification for inputs
+        CNEN1bits.CN6IE     = 1;
+        CNEN2bits.CN24IE    = 1;
+        CNEN2bits.CN27IE    = 1;
+
+        //Enable the internal pullup resistor for inputs
+        CNPU1bits.CN6PUE    = 1;
+        CNPU2bits.CN24PUE   = 1;
+        CNPU2bits.CN27PUE   = 1;
 }
 
 // ******************************************************************************************* //
